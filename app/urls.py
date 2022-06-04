0a205e43389c199939fac04d3a98cfa00628cef8
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-04 09:19:40 +0300

    st:project-structure - configure templates and app urls

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns=[
+    path('', views.app_home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.app_home, name='login')
+]
