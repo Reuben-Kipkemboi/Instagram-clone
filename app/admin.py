b9d1211900c8f9d55a18a0df8a2de003cc918038
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-04 15:29:34 +0300

    ft:models - creates application models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Instagram_post, User_comment, User_likes
 
 # Register your models here.
+admin.site.register(Profile),
+admin.site.register(Instagram_post),
+admin.site.register(User_likes),
+admin.site.register(User_comment)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,47 @@
+#imports
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 #APPLICATION MODELS
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=False)
+    image = CloudinaryField('profile_pic')
+    date_joined= models.DateField()
+    
+    # def __str__(self):
+    #     return self.user
+    
 
+class Instagram_post(models.Model):
+    title =models.CharField(max_length=200, null=False)
+    caption= models.CharField(max_length=500, null=True)
+    creator= models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted= models.DateField()
+    profile_of_creator= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+
+
+    
+class User_likes(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Instagram_post, on_delete=models.CASCADE)
+    
+    
+class User_comment(models.Model):
+    content = models.TextField( null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Instagram_post, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content
+    
+    
 
 
 
