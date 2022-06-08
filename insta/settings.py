59f83d61e3d5a7b0005a1785fa3d13f7f5e8fb60
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-08 14:38:24 +0300

    pt:email - patch to fix email error

--- insta/settings.py
+++ insta/settings.py
@@ -25,17 +25,12 @@ cloudinary.config(
 )
 
 
-EMAIL_USE_TLS=True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='kipkemboireuben866@gmail.com'
-#testreuby@yahoo.com
-EMAIL_HOST_PASSWORD='ymcyocprizxcolyx'
-
-#fsiazmuajpdqtbka
-
-
-
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
