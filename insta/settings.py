e0c03ed993c52379605a107c1eca3eada0e1ee3a
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-08 14:41:41 +0300

    pt:email - patch to fix email error

--- insta/settings.py
+++ insta/settings.py
@@ -24,6 +24,12 @@ cloudinary.config(
   api_secret = "zxr3TbjmRS2ZvPW9XT-sApQDEtA" 
 )
 
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='kipkemboireuben866@gmail.com'
+EMAIL_HOST_PASSWORD='ymcyocprizxcolyx'
+
 
 # Email configurations remember to install python-decouple
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
